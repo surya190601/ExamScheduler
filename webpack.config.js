58b676090f991241ccfc558330ab0e879cd32e76
surya190601 <111383074+surya190601@users.noreply.github.com>
2022-09-07 14:37:35 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   ],
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     clean: true,
     publicPath: "/",
   },
